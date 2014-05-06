0bb4c24634dd0fc53f575d2d264ee02817110d2e
kontodos <jcy_424@yahoo.com>
2014-05-06 23:21:09 +0800

    5/6/14
    things left to do:
    1. if quantity < quantity to be purchased
    2. if bills paid < amt due
    3. design

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './boot.rb'
-require './money_calculator'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -46,6 +46,18 @@ end
 # ROUTES FOR ADMIN SECTION
 
 get '/' do
+	product = Item.all
+	
+	#create an array full of names
+	itemNames = []
+		product.each do |item|
+			itemNames << item.name
+		end
+	
+	#create an array of 10 random items from the array of names
+	nameSample = itemNames.sample(10)
+	
+	@nameSample = nameSample
 	@product = Item.all
 	erb :index
 end
@@ -62,8 +74,23 @@ end
 
 post '/buy_product/:id' do
 	@product = Item.find(params[:id])
-	mc = MoneyCalculator.New(params[:one_coin], params[:five_coin], params[:ten_coin], params[:twenty_bill], params[:fifty_bill], params[:one_hundred_bill], params[:two_hundred_bill], params[:five_hundred_bill], params[:one_thousand_bill])
-	erb :purchase_form
+	mc = MoneyCalculator.new params[:one_coin], params[:five_coin], params[:ten_coin], params[:twenty_bill], params[:fifty_bill], params[:one_hundred_bill], params[:two_hundred_bill], params[:five_hundred_bill], params[:one_thousand_bill]
+	@given = mc.total #gets the total payment of the user
+	@quantity = params[:aquantity] #gets the quantity the user bought
+	@title = @product.name #gets item name
+	@due = @quantity.to_i * @product.price #gets total amount due
+	#@due = params[:due] code doesn't work
+	mc.getChange(@due) #performs money changer function
+	@change = @given - @due #change in mc just gives me 0 :(
+	@hash = mc.bills
+	#product.quantity = @product.quantity.to_i - @quantity.to_i
+	@left = @product.quantity.to_i - @quantity.to_i
+	@product.update_attributes!(
+	quantity: @left
+	)
+	erb :confirm
+	
+	
 end
 
 
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,6 +1,8 @@
 class MoneyCalculator
 
-		attr_accessor :total
+		attr_accessor :total, :bills, :change
+		attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :two_hundreds, :five_hundreds, :thousands
+		
 
   def initialize(ones, fives, tens, twenties, fifties, hundreds, two_hundreds, five_hundreds, thousands)
     # each parameter represents the quantity per denomination of money
@@ -10,54 +12,82 @@ class MoneyCalculator
     # and returns a hash of how much change is to be given
     # the hash will use the denominations as keys and the amount per denomination as values
     # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
-	@ones = ones * 1
-	@fives = fives * 5
-	@tens = tens * 10
-	@twenties = twenties * 20
- 	@fifties = fifties * 50
-	@hundreds = hundreds * 100
-	@two_hundreds = two_hundreds * 200
-	@five_hundreds = five_hundreds * 500
-	@thousands = thousands * 1000
+	@ones = ones.to_i * 1
+	@fives = fives.to_i * 5
+	@tens = tens.to_i * 10
+	@twenties = twenties.to_i * 20
+ 	@fifties = fifties.to_i * 50
+	@hundreds = hundreds.to_i * 100
+	@two_hundreds = two_hundreds.to_i * 200
+	@five_hundreds = five_hundreds.to_i * 500
+	@thousands = thousands.to_i * 1000
 	
-	total= @ones + @fives + @tens + @twenties + @fifties + @hundreds + @two_hundreds + @five_hundreds + @thousands
-	bills = Hash.new
+	@total = @ones.to_i + @fives.to_i + @tens.to_i + @twenties.to_i + @fifties.to_i + @hundreds.to_i + @two_hundreds.to_i + @five_hundreds.to_i + @thousands.to_i
+	
+	#@bills = {"thousands" => 0, "five_hundreds" => 0, "two_hundreds" => 0, "hundreds" => 0, "fifties" => 0, "twenties" => -0, "tens" => 0, "fives" => 0, "ones" => 0}
+	
+	@bills = Hash.new
+	
+	@bills["thousands"] = 0
+	@bills["five_hundreds"] = 0
+	@bills["two_hundreds"] = 0
+	@bills["hundreds"] = 0
+	@bills["fifties"] = 0
+	@bills["twenties"] = 0
+	@bills["tens"] = 0
+	@bills["fives"] = 0
+	@bills["ones"] = 0
 	
   end
   
-  def change(cost)
-	change = total - cost
+  
+  
+  def getChange(cost)
+	@change = @total - cost.to_i
+	changeToBills()
+  end
+  
+  def changeToBills()
+  
+	while @change > 0
 	
-	while change > 0
-	
-		if change - 1000 >= 0
+		if @change - 1000 >= 0
 			bills["thousands"] += 1
+			@change = @change - 1000
 		
-		elsif change - 500 >= 0
+		elsif @change - 500 >= 0
 			bills["five_hundreds"] += 1
-		
-		elsif change - 200 >= 0
-			bills["two_hundreds"] += 1
+			@change = @change - 500
 			
-		elsif change - 100 >= 0
+		elsif @change - 200 >= 0
+			bills["two_hundreds"] += 1
+			@change = @change - 200
+			
+		elsif @change - 100 >= 0
 			bills["hundreds"] += 1
-		
-		elsif change - 50 >= 0
-			bills["fifties"] += 1
-		
-		elsif change - 20 >= 0
-			bills["twenties"] += 1
-		
-		elsif change - 10 >= 0
+			@change = @change - 100
+			
+		elsif @change - 50 >= 0
+			bills["fifties"] = bills["fifties"].to_i + 1
+			@change = @change - 50
+			
+		elsif @change - 20 >= 0
+			bills["twenties"] = bills["twenties"].to_i + 1
+			@change = @change - 20
+			
+		elsif @change - 10 >= 0
 			bills["tens"] += 1
-		
-		elsif change - 5 >= 0
+			@change = @change - 10
+			
+		elsif @change - 5 >= 0
 			bills["fives"] += 1
+			@change = @change - 5
 			
 		else
 			bills["ones"] += 1
+			@change = @change - 1
 		end
-  end
+	end
 end
 
 end
